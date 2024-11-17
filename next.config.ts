5777fcded77ed4e587f669b4e4f1cc6b670e3ee4
cristinaAlarcon98 <cristinaalarconjimenez@gmail.com>
2024-11-17 16:09:31 +0100

    deleted unused fonts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      exclude: /node_modules\/(?!gsap)/, // Exclude gsap from being transpiled
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['next/babel'],
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
