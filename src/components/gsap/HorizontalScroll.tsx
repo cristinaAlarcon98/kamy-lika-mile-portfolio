45cb55308f5b70f11358597b2543ca12ed816150
cristinaAlarcon98 <cristinaalarconjimenez@gmail.com>
2024-11-17 23:05:24 +0100

    feat: changed to a more responsive design

--- src/components/gsap/HorizontalScroll.tsx
+++ src/components/gsap/HorizontalScroll.tsx
@@ -45,19 +45,18 @@ function HorizontalScroll() {
   ); 
 
   return (
-    <div className="flex flex-col md:flex-row ">
-         <div className="w-full md:w-5/12 mt-5 md:mt-0  pr-[10%] ">
-          
+    <div className="flex  md:flex-row ">
+         <div className="flex-col md:w-5/12 mt-5 md:mt-0 pr-[10%] ">
              <div className="text-element flex items-end h-[60vh]">
-             <div className='absolute bottom-[-150px] left-[-23rem] z-5 w-[80vh] h-[80vh]'>
-                <BlobCircle fillColor="#5781b7"  ></BlobCircle>
-          </div>
-            <p  className="text-white text-right  z-10">
-            The emotionality of the <span>artistic</span> temperament. My field is <span>sports</span>, but my thinking is <span>artistic</span>. These two niches combined into one can reveal <span>undiscovered layers</span>.
-            </p>
-            </div> 
+                <div className='absolute bottom-[-150px] left-[-23rem] z-5 w-[80vh] h-[80vh]'>
+                   <BlobCircle fillColor="#5781b7"  ></BlobCircle>
+                </div>
+                <p className="text-white text-right  z-10">
+                  The emotionality of the <span>artistic</span> temperament. My field is <span>sports</span>, but my thinking is <span>artistic</span>. These two niches combined into one can reveal <span>undiscovered layers</span>.
+                </p>
+             </div> 
         </div> 
-         <div  className="square w-full md:w-7/12  pl-[5%] "> 
+         <div  className="square flex-col md:w-7/12  pl-[5%] "> 
             <div className="slides-container overflow-hidden ">
                 <div className="slides flex space-x-4 ">
                     <img src="/images/volley.png" alt="Volleyball Image" className="h-[60vh] w-auto" />
