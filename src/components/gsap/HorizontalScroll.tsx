a65f8684bf7210c679217225e52fb5e58dd7da11
cristinaAlarcon98 <cristinaalarconjimenez@gmail.com>
2024-11-17 17:38:32 +0100

    feat: added use GSAP

--- src/components/gsap/HorizontalScroll.tsx
+++ src/components/gsap/HorizontalScroll.tsx
@@ -1,64 +1,57 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "./esm/ScrollTrigger";
+import { useGSAP } from '@gsap/react';
 
+gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 
-const HorizontalScroll = () => {
-
-  const slidesContainerRef = useRef(null);
-  const slidesRef = useRef(null);
-  const textRef = useRef(null);
-
-  useEffect(() => {
-      const slidesContainerElemet = slidesContainerRef.current;
-      const slidesElement = slidesRef.current
-      const textElement = textRef.current;
-
-     gsap.to(slidesElement, {
-        x: -(850),
-        duration:10000,
-        scrollTrigger: {
-          trigger: slidesContainerElemet,
-          start: "center center",
-          end: "bottom center",     
-          scrub: 1,
-          pin: true,
-          markers: true
-        }
-      }); 
-
-      gsap.to(textElement, {
-       // x: -(850),
-        scrollTrigger: {
-          trigger: textElement,
-          start: "center center",
-          end: "bottom center",     
-          scrub: 1,
-          pin: true,
-         // markers: true
-        }
-      }); 
-
-     
-     
 
 
-     
-    }, []);
+function HorizontalScroll() {
+
+    useGSAP(
+      () => {
+          // gsap code here...
+          gsap.to(".slides", {
+            x: -(850),
+            scrollTrigger: {
+              trigger: ".slides-container",
+              start: "center center",
+              end: "bottom center",     
+              scrub: 1,
+              pin: true,
+              markers: true
+            }
+          }); 
+    
+          gsap.to(".text-element", {
+           // x: -(850),
+            scrollTrigger: {
+              trigger: ".text-element",
+              start: "center center",
+              end: "bottom center",     
+              scrub: 1,
+              pin: true,
+             // markers: true
+            }
+          }); 
+      },
+      { }
+  ); 
 
   return (
     <div className="flex flex-col md:flex-row ">
          <div className="w-full md:w-5/12 mt-5 md:mt-0  pr-[10%] ">
-             <div ref={textRef}  className="flex items-end h-[60vh]">
+             <div className="text-element flex items-end h-[60vh]">
             <p  className="text-white text-right ">
             The emotionality of the <span>artistic</span> temperament. My field is <span>sports</span>, but my thinking is <span>artistic</span>. These two niches combined into one can reveal <span>undiscovered layers</span>.
             </p>
             </div> 
         </div> 
          <div  className="square w-full md:w-7/12  pl-[5%] "> 
-            <div ref={slidesContainerRef} className="overflow-hidden ">
-                <div ref={slidesRef}className="slides flex space-x-4 ">
+            <div className="slides-container overflow-hidden ">
+                <div className="slides flex space-x-4 ">
                     <img src="/images/volley.png" alt="Volleyball Image" className="h-[60vh] w-auto" />
                     <img src="/images/volley.png" alt="Volleyball Image" className="h-[60vh] w-auto" />
                     <img src="/images/volley.png" alt="Volleyball Image" className="h-[60vh] w-auto" />
